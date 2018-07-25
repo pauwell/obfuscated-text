5732f2cab5d1aa08dd1338867991b56668d5f3fb
Paul Bernitz <paulbernitz@web.de>
2018-07-25 19:36:27 +0200

    Update string_art_standard.c
    Fixed formatting.

--- string_art_standard.c
+++ string_art_standard.c
@@ -34,14 +34,16 @@
 */
 
 // Letters (all flipped horizontal).
-const unsigned int LETTERS[] = { 0xF9F99/* A */, 0x79797/* B */, 0xF111F/* C */, 0x79997/* D */, 
-							     0xF1F1F/* E */, 0xF1F11/* F */, 0xE1D9F/* G */, 0x99F99/* H */,
-							     0x22222/* I */, 0x3119E/* J */, 0x95159/* K */, 0x1111F/* L */,
-							     0x9FF99/* M */, 0x9BFD9/* N */, 0xF999F/* O */, 0x79711/* P */,
-							     0xF99F8/* Q */, 0x79759/* R */, 0x61686/* S */, 0xF4444/* T */,
-							     0x9999F/* U */, 0x99996/* V */, 0x99FF9/* W */, 0x99699/* X */,
-							     0x99644/* Y */, 0xF861F/* Z */};
-
+const unsigned int LETTERS[] = 
+{
+	0xF9F99/* A */, 0x79797/* B */, 0xF111F/* C */, 0x79997/* D */, 
+	0xF1F1F/* E */, 0xF1F11/* F */, 0xE1D9F/* G */, 0x99F99/* H */,
+	0x22222/* I */, 0x3119E/* J */, 0x95159/* K */, 0x1111F/* L */,
+	0x9FF99/* M */, 0x9BFD9/* N */, 0xF999F/* O */, 0x79711/* P */,
+	0xF99F8/* Q */, 0x79759/* R */, 0x61686/* S */, 0xF4444/* T */,
+	0x9999F/* U */, 0x99996/* V */, 0x99FF9/* W */, 0x99699/* X */,
+	0x99644/* Y */, 0xF861F/* Z *//* Todo: Add special characters.*/
+};
 
 /*
 	Reads two arguments:
@@ -66,18 +68,12 @@ int main(int argc, char* argv[])
 
 	// Store each letter bitwise in the "result_rows".
 	for(ch=0; ch<strlen(argv[1]); ch++)
-	{
 		for(row=5; row-->0;)
-		{
 			for(bit=0; bit<4; bit++)
-			{
 				if (LETTERS[(tolower(argv[1][ch])) - 97] & (1 << (row*4+bit)))
 					result_rows[row][ch * 4 + bit] = 0xB6;
 				else 
 					result_rows[row][ch * 4 + bit] = 0xA0;
-			}
-		}
-	}
 
 	// Write as characters into text-file.
 	FILE* file = fopen(argv[2], "w");
